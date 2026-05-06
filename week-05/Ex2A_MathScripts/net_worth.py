1d86451e66ca4435830003ecd3542b558280147c
Hana Tilahun <hanamakeda04@email.com>
2026-05-06 09:36:40 -0600

    2nd pyth file

---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,26 @@
+# Define values
+total_assets = 15000
+total_debts = 5000
+
+# Calculate net worth
+net_worth = total_assets - total_debts
+
+# Output
+print("Your total assets are " + str(total_assets))
+print("Your total debts are " + str(total_debts))
+print("Your net worth is " + str(net_worth))
+#part2
+# Define sides (example birthday: May 12)
+side_a = 5   # month
+side_b = 12  # day
+
+# Calculate area
+area = side_a * side_b
+
+# Output
+print("Side A is " + str(side_a))
+print("Side B is " + str(side_b))
+print("The area of the rectangle is " + str(area))
+#a) Area formula = length × width
+#b) Script = above
+#c) Output format = matches exactly what they asked
