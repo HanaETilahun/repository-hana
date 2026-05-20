487d884920cc37aadbae45d9472a4739cd11f4c0
Hana Tilahun <hanamakeda04@email.com>
2026-05-20 16:13:35 -0600

    Complete Ex4A read write files

---
+++ week-06/Ex4A_ReadWriteFiles/open_and_write.py
@@ -0,0 +1,5 @@
+f = open("about_me.txt", "a")
+
+f.write("\nIf I could do anything for my perfect night out, I would go to a nice restaurant, enjoy good food, listen to music, and spend time with people I love.")
+
+f.close()
